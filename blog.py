de9117bb3b9ed1fcb817adf5c0d7566ff08d0496
alex <alex@hrck.net>
2016-01-21 19:07:17 -0600

    better timestamp handling

--- blog.py
+++ blog.py
@@ -3,26 +3,50 @@ import markdown
 from flask import Flask
 from flask import render_template
 from flask import Markup
-from flask import send_from_directory
 import glob
 from random import randint
-import os
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     entries = list()
+    archives = list()
     posts = glob.glob('posts/*.md')
-    posts.sort(key=lambda x: os.path.getmtime(x), reverse=True)
-    for post in posts:
-      with open(post, 'rb') as f:
-        content = dict()
-        content['id'] = randint(0,1000)
-        content['text'] = Markup(markdown.markdown(unicode(f.read(), 'utf8'), ['markdown.extensions.extra']))
-      entries.append(content)
+    for post in sorted(posts, reverse=True):
+        with open(post, 'rb') as f:
+            ts = f.name.split('/')[1].split('-')[0]  # get the date stamp from the posts filename
+            arc_date = datetime.strptime(ts, "%Y%m%d")
+            
+            if arc_date not in archives:
+                archives.append(arc_date)
 
-    return render_template('index.html', entries=entries)
+            p = f.read()
+            content = dict()
+            content['id'] = randint(0,1000)
+            content['text'] = Markup(markdown.markdown(unicode(p, 'utf8'), ['markdown.extensions.extra']))
+        entries.append(content)
+
+    return render_template('index.html', entries=entries, archives=sorted(archives, reverse=True))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+### Code to convert dates in posts to dates used in filename
+#from datetime import datetime
+#post_date = p.split('\n')[2]
+#fmt_post_date = list()
+#try:
+#    s = datetime.strptime(post_date, "%B %d, %Y")
+#    new_date = s.strftime("%Y%m%d")
+#    fmt_post_date.append(new_date)
+#except ValueError:
+#    print f.name
+#filename = new_date + '-' + f.name.split('/')[1]
+#with open(filename, 'wb') as nf:
+#    nf.write(p)
+#print sorted(fmt_post_date, reverse=True)
--- posts/WIPFW Windows IP Firewall.md
+++
@@ -1,9 +0,0 @@
-##WIPFW Windows IP Firewall {:.blog-post-title}
-
-March 23, 2010
-{:.blog-post-meta}
-
-
-For those looking for a full featured, scriptable and light weight firewall for Windows XP and Sever 2003 (or earlier) check out WinIPFW (http://wipfw.sourceforge.net). While versions on Windows prior to Vista have a built in firewall, it is limiting. The Windows firewall will not filter egress traffic and has poor logging capabilities. Fine grain filtering control of IP address, protocol and port of both ingress and egress traffic are all features of WinIPFW. Advanced logging of any desired traffic is possible as is the ability to create Windows batch script files for automatic configuration.
-
-If you install WinIPFW blocking all in-bound and out-bound traffic you must specifically open ports for web browsing, etc. Also, always install and configure firewalls using a local console to avoid blocking remote administration services (SSH,VNC,RDP).
