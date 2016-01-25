1f6aa22494f802eeea2b8ae4ca943595c592030f
Alex Herrick <alex@hrck.net>
2016-01-24 19:55:46 -0600

    updates

--- blog.py
+++ blog.py
@@ -3,8 +3,11 @@ from flask import Flask
 from flask import render_template
 from flask import Markup
 import glob
+from flask import url_for
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+import collections
+from flask import redirect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # If not needed. Set to True uses more memory
@@ -29,7 +32,7 @@ class Post(db.Model):
 
 def add_blog_entry():
     mdfiles = glob.glob('posts/*.md')
-    for md in sorted(mdfiles, reverse=True):
+    for md in mdfiles:
         with open(md, 'rb') as f:
             for i, line in enumerate(f):
                 if i == 0:
@@ -40,52 +43,71 @@ def add_blog_entry():
         with open(md, 'rb') as f:
             a = unicode(f.read(), 'utf8')
             text = Markup(markdown.markdown(a, ['markdown.extensions.extra']))
-
-        p = Post(date, title, text)
-        db.session.add(p)
+        if not Post.query.filter_by(title=title).first():
+            p = Post(date, title, text)
+            db.session.add(p)
     try:
         db.session.commit()
     except Exception as e:
-        pass
+        return 'no updates'
+    return 'OK'
 
-@app.route('/')
-def index():
-    entries = dict()
+
+def archives_sidebar():
     archives = list()
-
     posts = Post.query.all()
-    for post in posts:
-        #''.join(post.text.split('\n')[0:4])
-        entries[post.id] = post.text
-        archives.append(post.date)
+    for p in posts:
+        a = p.date.date().strftime("%Y")
+        if a not in archives:
+            archives.append(a)
+    return sorted(archives, reverse=True)
 
-    return render_template('index.html', entries=entries, archives=sorted(archives, reverse=True))
+
+@app.route('/<int:page>')
+@app.route('/', defaults={'page': 1})
+def index(page):
+    e = dict()
+    archives = list()
+    next_pg = page + 1
+    prev_pg = page - 1
+    pages = Post.query.order_by(Post.id.desc()).paginate(page, 4, False)
+    if page > pages.pages:
+        return redirect(url_for('index'))
+
+    for post in pages.items:
+        e[post.id] = post.text
+        archives.append(post.date)
+    entries = collections.OrderedDict(sorted(e.items(), reverse=True))
+    return render_template('index.html', entries=entries, archives=archives_sidebar(), next_pg=next_pg, prev_pg=prev_pg)
 
 
 @app.route('/posts/<int:id>')
 def expand_post(id=None):
     post = Post.query.filter_by(id=id).first()
 
-    return render_template('expand.html', post=post.text)
+    archives = list()
+    posts = Post.query.all()
+    for p in posts:
+        a = p.date.date().strftime("%Y")
+        if a not in archives:
+            archives.append(a)
+
+    return render_template('expand.html', post=post.text, archives=archives_sidebar())
+
+@app.route('/<year>')
+def archives_by_year(year=None):
+    posts = Post.query.filter(Post.date.like('%' + year + '%')).all()
+    entries = dict()
+    for post in posts:
+        entries[post.id] = post.text
+    entries = collections.OrderedDict(sorted(entries.items(),  reverse=True))
+    return render_template('index.html', entries=entries, archives=archives_sidebar())
+
+@app.route('/update')
+def update_blog():
+    add_blog_entry()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
-
-
-
-
-### Code to convert dates in posts to dates used in filename
-#from datetime import datetime
-#post_date = p.split('\n')[2]
-#fmt_post_date = list()
-#try:
-#    s = datetime.strptime(post_date, "%B %d, %Y")
-#    new_date = s.strftime("%Y%m%d")
-#    fmt_post_date.append(new_date)
-#except ValueError:
-#    print f.name
-#filename = new_date + '-' + f.name.split('/')[1]
-#with open(filename, 'wb') as nf:
-#    nf.write(p)
-#print sorted(fmt_post_date, reverse=True)
